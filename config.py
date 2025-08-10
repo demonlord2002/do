af79df124525869d8d521f2d9139d43aa888200b
demonlord2002 <drdoom2003p@gmail.com>
2025-08-10 19:21:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # ==== BOT CONFIG ====
 API_ID = int(os.getenv("API_ID", "23559126"))
 API_HASH = os.getenv("API_HASH", "58347a441c011b1b9ee3367ea936dcc4")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7607268654:AAGup94GSmRa3U_SQ604_2XgKOovWsueYsM")
 
 # ==== MONGODB CONFIG ====
 MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://drdoom2003p:drdoom2003p@cluster0.fnhjrtn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
